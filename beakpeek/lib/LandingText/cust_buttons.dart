60996a700f0265b010a22dc67f0870c25211dad5
JarodJeffery <JarodJeffery@gmail.com>
2024-06-01 22:23:37 +0200

    tests

---
+++ beakpeek/lib/LandingText/cust_buttons.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class CustButtons extends StatelessWidget {
+  const CustButtons(this.path, this.textD, this.c, this.tc, {super.key});
+
+  final String path;
+  final String textD;
+  final Color c;
+  final Color tc;
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () {
+        Navigator.pushNamed(context, path);
+      },
+      style: FilledButton.styleFrom(
+        backgroundColor: c,
+        minimumSize: const Size(350, 50),
+        shadowColor: Colors.black,
+      ),
+      child: Text(
+        textD,
+        style: TextStyle(
+          color: tc,
+        ),
+      ),
+    );
+  }
+}
+
+class CustOutlinedButton extends StatelessWidget {
+  // ignore: lines_longer_than_80_chars
+  const CustOutlinedButton(this.onclick, this.textD, this.c, this.tc,
+      {super.key});
+
+  final void Function() onclick;
+  final String textD;
+  final Color c;
+  final Color tc;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SizedBox(
+          width: constraints.maxWidth / 3, // Match the parent's width
+          child: OutlinedButton(
+            onPressed: onclick,
+            style: OutlinedButton.styleFrom(
+              backgroundColor: c,
+              foregroundColor: c,
+            ),
+            child: Text(
+              textD,
+              style: TextStyle(
+                color: tc,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ beakpeek/lib/log_in.dart
@@ -0,0 +1,40 @@
+import 'package:aad_b2c_webview/aad_b2c_webview.dart';
+import 'package:beakpeek/LandingText/login_stack.dart';
+import 'package:flutter/material.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({super.key});
+
+  @override
+  State<LogIn> createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  String? jwtToken;
+  String? refreshToken;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F1ED),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const LoginStack(),
+            IconButton(
+              icon: Image.asset('assets/icons/google.png'),
+              onPressed: () {},
+              tooltip: 'Sign in with google',
+            ),
+            IconButton(
+              icon: Image.asset('assets/icons/facebook.png'),
+              onPressed: () {},
+              tooltip: 'Sign in with google',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
