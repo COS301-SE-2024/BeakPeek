e9e549181dccc6008d5e3fa6607ffe26ef434e62
Justin Hudson <u21543152@tuks.co.za>
2024-06-01 17:45:44 +0200

    Added a new folder and images

--- beakpeek/lib/LandingText/cust_buttons.dart
+++ beakpeek/lib/LandingText/cust_buttons.dart
@@ -25,3 +25,35 @@ class CustButtons extends StatelessWidget {
     );
   }
 }
+
+class CustOutlinedButton extends StatelessWidget {
+  const CustOutlinedButton(this.onclick, this.textD, this.c, this.tc, {super.key});
+
+  final void Function() onclick;
+  final String textD;
+  final Color c;
+  final Color tc;
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SizedBox(
+          width: constraints.maxWidth/3, // Match the parent's width
+          child: OutlinedButton(
+            onPressed: onclick,
+            style: OutlinedButton.styleFrom(
+              backgroundColor: c,
+              foregroundColor: c,
+            ),
+            child: Text(
+              textD,
+              style: TextStyle(
+                color: tc,
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
