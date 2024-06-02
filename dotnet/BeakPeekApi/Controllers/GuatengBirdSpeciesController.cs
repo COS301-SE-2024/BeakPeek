6f4e98ac47197ae7e4d09568fad8da70c54b46b4
ChuufMaster <ivanhorak20@gmail.com>
2024-06-02 13:36:55 +0200

    Got the dotnet api to work with docker compose and the database

--- dotnet/BeakPeekApi/Controllers/GuatengBirdSpeciesController.cs
+++ dotnet/BeakPeekApi/Controllers/GuatengBirdSpeciesController.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BeakPeekApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BeakPeekApi.Controllers
 {
@@ -21,10 +24,10 @@ namespace BeakPeekApi.Controllers
             return await _context.GautengBirdSpecies.ToListAsync();
         }
 
-        [HttpGet("{Genus}")]
-        public async Task<ActionResult<GautengBirdSpecies>> GetGautengBirdSpecies(string Genus)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GautengBirdSpecies>> GetGautengBirdSpecies(int id)
         {
-            var species = await _context.GautengBirdSpecies.FindAsync(Genus);
+            var species = await _context.GautengBirdSpecies.FindAsync(id);
 
             if (species == null)
             {
@@ -32,6 +35,30 @@ namespace BeakPeekApi.Controllers
             }
 
             return species;
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<GautengBirdSpecies>>> SearchGautentBirdSpecies(string genus = null, string commonSpecies = null)
+        {
+            IQueryable<GautengBirdSpecies> query = _context.GautengBirdSpecies;
+
+            if (!string.IsNullOrEmpty(genus))
+            {
+                query = query.Where(b => EF.Functions.Like(b.Genus, $"%{genus}%"));
+            }
+
+            if (!string.IsNullOrEmpty(commonSpecies))
+            {
+                query = query.Where(b => EF.Functions.Like(b.Common_species, $"%{commonSpecies}%"));
+            }
+
+            var results = await query.ToListAsync();
+
+            if (!results.Any())
+            {
+                return NotFound();
+            }
+            return results;
         }
 
         [HttpPost]
