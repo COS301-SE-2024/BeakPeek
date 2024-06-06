84d168df7fcff93b3e3475c053fc1665ce578536
ChuufMaster <ivanhorak20@gmail.com>
2024-06-06 18:54:39 +0200

    Added tests for the bird.dart file in flutter

--- dotnet/BeakPeekApi.Tests/Controllers/GuatengBirdSpeciesControllerTest.cs
+++ dotnet/BeakPeekApi.Tests/Controllers/GuatengBirdSpeciesControllerTest.cs
@@ -66,6 +66,44 @@ namespace BeakPeekApi.Tests.Controllers
 
         }
 
-        // Add more tests here for other methods in your controller
+        [Fact]
+        public async Task PostGautengBirdSpecies_ReturnsSpecies_WhenPosted()
+        {
+
+            var species = new GautengBirdSpecies { Pentad = "1" };
+            _mockContext.Setup(x => x.GautengBirdSpecies.Add(species));
+            _mockContext.Setup(x => x.SaveChangesAsync(default(CancellationToken))).ReturnsAsync(1);
+
+            var result = await _controller.PostGautengBirdSpecies(species);
+
+            var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+            Assert.Equal("GetGautengBirdSpecies", createdAtActionResult.ActionName);
+            Assert.Equal(species, createdAtActionResult.Value);
+            Assert.Equal(species.Pentad, ((GautengBirdSpecies)createdAtActionResult.Value).Pentad);
+
+        }
+
+        [Fact]
+        public async Task DeleteGautengBirdSpecies_ReturnsNoContent_WhenIdExists()
+        {
+            var species = new GautengBirdSpecies { Pentad = "1" };
+            _mockContext.Setup(x => x.GautengBirdSpecies.FindAsync("1")).ReturnsAsync(species);
+            _mockContext.Setup(x => x.GautengBirdSpecies.Remove(species));
+            _mockContext.Setup(x => x.SaveChangesAsync(default(CancellationToken))).ReturnsAsync(1);
+
+            var result = await _controller.DeleteGautengBirdSpecies("1");
+
+            Assert.IsType<NoContentResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteGautengBirdSpecies_ReturnsNotFound_WhenIdDoesNotExist()
+        {
+            _mockContext.Setup(x => x.GautengBirdSpecies.FindAsync("1")).ReturnsAsync((GautengBirdSpecies)null);
+
+            var result = await _controller.DeleteGautengBirdSpecies("1");
+
+            Assert.IsType<NotFoundResult>(result);
+        }
     }
 }
