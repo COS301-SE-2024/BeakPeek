90b37c64bec3dd51b8ca4c667f28af20294819d1
Matt <mattgbotha@gmail.com>
2024-06-14 11:15:56 +0200

    Fixed routing and imports

--- beakpeek/lib/main.dart
+++ beakpeek/lib/main.dart
@@ -1,6 +1,21 @@
-import 'package:beakpeek/Controller/router_provider.dart';
+import 'package:beakpeek/view/home/bird_search.dart';
+import 'package:beakpeek/view/home/home.dart';
+import 'package:beakpeek/view/login/landing_page.dart';
+import 'package:beakpeek/log_in.dart';
+import 'package:beakpeek/view/home/map_info.dart';
+import 'package:beakpeek/sign_up.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const RouterProvider());
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: <String, WidgetBuilder>{
+      '/': (context) => const LandingPage(),
+      '/login': (context) => const LogIn(),
+      '/signup': (context) => const SignUp(),
+      '/home': (context) => const Home(),
+      '/birdSearch': (context) => const BirdSearch(),
+      '/map': (context) => const MapInfo(),
+    },
+  ));
 }
