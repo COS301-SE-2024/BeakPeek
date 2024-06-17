3c2cc22cc39fa390b1a8a088b1ee890bf9b5548f
Matt <mattgbotha@gmail.com>
2024-06-17 13:24:16 +0200

    Improved structure and layout of landing tabs

--- beakpeek/lib/View/Login/landing_tab_2.dart
+++ beakpeek/lib/View/Login/landing_tab_2.dart
@@ -1,53 +1,70 @@
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:flutter/material.dart';
 import 'package:beakpeek/LandingText/cust_buttons.dart';
 import 'package:beakpeek/LandingText/cust_text_span.dart';
 import 'package:beakpeek/Styles/landing_page_styles.dart';
-import 'package:flutter/material.dart';
 
 class LandingTab2 extends StatelessWidget {
   const LandingTab2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Align(
-        alignment: Alignment.bottomCenter,
+    return Scaffold(
+      backgroundColor: const Color(0xffF0EDE6),
+      body: SafeArea(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          verticalDirection: VerticalDirection.up,
           children: [
+            Expanded(
+              child: Column(
+                children: [
+                  const Spacer(flex: 5),
+                  Align(
+                    alignment: const Alignment(-0.7, 0.0),
+                    child: RichText(
+                      textAlign: TextAlign.left,
+                      text: const TextSpan(
+                        style: LandingStyles.loginHeadingDark,
+                        children: [
+                          TextSpan(
+                            text: 'Explore,\n',
+                          ),
+                          TextSpan(
+                            text: 'Discover,\n',
+                            style: LandingStyles.loginHeadingYellow,
+                          ),
+                          TextSpan(
+                            text: 'and Share',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment(-0.5, 0.0),
+                    child: CustTextSpan(),
+                  ),
+                  const Spacer(flex: 3),
+                  const CustButtons(
+                    '/signup',
+                    'Sign Up',
+                    Colors.black,
+                    Colors.white,
+                  ),
+                  const SizedBox(height: 20),
+                  const CustButtons(
+                    '/login',
+                    'Sign In',
+                    Colors.white,
+                    Colors.black,
+                  ),
+                ],
+              ),
+            ),
             Image.asset(
               'assets/images/landing2Resize.png',
               fit: BoxFit.fitWidth,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const CustButtons('/login', 'Login', Colors.black, Colors.white),
-            const SizedBox(
-              height: 20,
-            ),
-            const CustButtons('/signup', 'Sign Up', Colors.white, Colors.black),
-            const SizedBox(
-              height: 20,
-            ),
-            const Align(
-              alignment: FractionalOffset(0.15, 0.5),
-              child: CustTextSpan(),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Align(
-              alignment: const FractionalOffset(0.1, 0.5),
-              child: RichText(
-                text: const TextSpan(
-                  text: 'Explore, \nDiscover, \nAnd Share',
-                  style: LandingStyles.loginHeadingDark,
-                ),
-                textAlign: TextAlign.left,
-              ),
             ),
           ],
         ),
