d1e0681517ead94c1ccb9bd613d370bb0f65b827
JarodJeffery <JarodJeffery@gmail.com>
2024-06-19 20:08:05 +0200

    Filter Refactor and add need assistance

---
+++ beakpeek/lib/Module/bird_search_functions.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: avoid_print, unused_element
+import 'dart:convert';
+import 'package:beakpeek/Module/bird.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class BirdSearchFunctions {
+  Future<List<Bird>> fetchAllBirds(http.Client client) async {
+    try {
+      final response = await client
+          .get(Uri.parse('http://10.0.2.2:5000/api/GautengBirdSpecies'));
+      print(response);
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonResponse = json.decode(response.body);
+        return jsonResponse.map((data) => Bird.fromJson(data)).toList();
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+        throw Exception('Failed to load birds');
+      }
+    } catch (error) {
+      print('Error fetching birds: $error');
+      throw Exception('Failed to load birds: $error');
+    }
+  }
+
+  final colorArray = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+  ];
+
+  int getColorForReportingRate(double reportingRate) {
+    if (reportingRate < 40) {
+      return 0;
+    } else if (reportingRate < 60) {
+      return 1;
+    } else if (reportingRate < 80) {
+      return 2;
+    } else {
+      return 3;
+    }
+  }
+}
--- beakpeek/lib/main.dart
+++ beakpeek/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/',
+    initialRoute: '/birdSearch',
     routes: <String, WidgetBuilder>{
       '/': (context) => const LandingPage(),
       '/login': (context) => const LogIn(),
