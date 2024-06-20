e87ad7c0f660bbab727ce758ffd7228ec3d91467
Justin Hudson <u21543152@tuks.co.za>
2024-06-20 12:55:08 +0200

    Testing and Refactoring of Home page and Maps

--- beakpeek/test/home_test.dart
+++ beakpeek/test/home_test.dart
@@ -1,4 +1,5 @@
-import 'package:beakpeek/LandingText/nav.dart';
+import 'package:beakpeek/Module/nav.dart';
+import 'package:beakpeek/Module/top_search_bar.dart';
 import 'package:beakpeek/View/Home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
