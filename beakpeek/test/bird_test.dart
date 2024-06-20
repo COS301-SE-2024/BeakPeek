7a3bcd7fd8a5cccad9a359421e60ea1e665c49d5
Justin Hudson <u21543152@tuks.co.za>
2024-06-20 12:59:05 +0200

    Fixed Linking error

--- beakpeek/test/bird_test.dart
+++ beakpeek/test/bird_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
-import 'package:beakpeek/View/Home/bird.dart';
+import 'package:beakpeek/View/Home/bird_sheet.dart';
 // class MockClient extends Mock implements http.Client {}
 
 import 'bird_test.mocks.dart';
