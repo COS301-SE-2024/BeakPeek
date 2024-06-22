35e7ebda47cb146a6852d915cc8d906fa7b7e4bd
JarodJeffery <JarodJeffery@gmail.com>
2024-06-22 17:42:54 +0200

    Home test added

--- beakpeek/test/home_test.dart
+++ beakpeek/test/home_test.dart
@@ -5,23 +5,40 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Home widget test', () {
-    testWidgets('View Map button is displayed', (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: Home(),
-        ),
-      ));
-      expect(find.text('View Map'), findsOneWidget);
-    });
+  group(
+    'Home widget test',
+    () {
+      testWidgets('View Map button is displayed', (tester) async {
+        await tester.pumpWidget(const MaterialApp(
+          home: Scaffold(
+            body: Home(),
+          ),
+        ));
+        expect(find.text('View Map'), findsOneWidget);
+      });
 
-    testWidgets('Bottom navigation is displayed', (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: Home(),
-        ),
-      ));
-      expect(find.byType(BottomNavigation), findsOneWidget);
-    });
-  });
+      testWidgets('Bottom navigation is displayed', (tester) async {
+        await tester.pumpWidget(const MaterialApp(
+          home: Scaffold(
+            body: Home(),
+          ),
+        ));
+        expect(find.byType(BottomNavigation), findsOneWidget);
+      });
+
+      testWidgets(
+        'Search is displayed',
+        (tester) async {
+          await tester.pumpWidget(
+            const MaterialApp(
+              home: Scaffold(
+                body: Home(),
+              ),
+            ),
+          );
+          expect(find.byType(SearchbarContainer), findsOneWidget);
+        },
+      );
+    },
+  );
 }
