2cafcd6a1af00247ebb0bd1ceaf4f38b300cc7c7
Justin Hudson <u21543152@tuks.co.za>
2024-06-22 13:47:07 +0200

    Implemented the heat map for birds

--- beakpeek/test/home_test.dart
+++ beakpeek/test/home_test.dart
@@ -6,31 +6,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('Home widget test', () {
-    // WidgetTester tester;
-
-    testWidgets('Search bar is displayed', (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: Home(),
-        ),
-      ));
-      expect(find.byType(TopSearchBar), findsOneWidget);
-    });
-
-    // ignore: lines_longer_than_80_chars
-    // testWidgets("Justin's Bird of the Day text is displayed", (tester) async {
-    //   expect(find.text('Bird of the Day'), findsOneWidget);
-    // });
-
-    // testWidgets('Black Fronted Bushsrike text is displayed', (tester) async {
-    //   await tester.pumpWidget(const MaterialApp(
-    //     home: Scaffold(
-    //       body: Home(),
-    //     ),
-    //   ));
-    //   expect(find.text('Black Fronted Bushsrike'), findsOneWidget);
-    // });
-
     testWidgets('View Map button is displayed', (tester) async {
       await tester.pumpWidget(const MaterialApp(
         home: Scaffold(
