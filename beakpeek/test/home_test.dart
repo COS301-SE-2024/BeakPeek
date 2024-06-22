2e4e3cb0c3c5047f4fcc581c9e0d101bf8e173f0
JarodJeffery <JarodJeffery@gmail.com>
2024-06-22 17:42:54 +0200

    Home test added

--- beakpeek/test/home_test.dart
+++ beakpeek/test/home_test.dart
@@ -1,52 +1,70 @@
 import 'package:beakpeek/Model/nav.dart';
+import 'package:beakpeek/View/Home/Searching/searchbar_container.dart';
 //import 'package:beakpeek/Model/top_search_bar.dart';
 import 'package:beakpeek/View/Home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Home widget test', () {
-    // WidgetTester tester;
+  group(
+    'Home widget test',
+    () {
+      // WidgetTester tester;
 
-    // testWidgets('Search bar is displayed', (tester) async {
-    //   await tester.pumpWidget(const MaterialApp(
-    //     home: Scaffold(
-    //       body: Home(),
-    //     ),
-    //   ));
-    //   expect(find.byType(TopSearchBar), findsOneWidget);
-    // });
+      // testWidgets('Search bar is displayed', (tester) async {
+      //   await tester.pumpWidget(const MaterialApp(
+      //     home: Scaffold(
+      //       body: Home(),
+      //     ),
+      //   ));
+      //   expect(find.byType(TopSearchBar), findsOneWidget);
+      // });
 
-    // ignore: lines_longer_than_80_chars
-    // testWidgets("Justin's Bird of the Day text is displayed", (tester) async {
-    //   expect(find.text('Bird of the Day'), findsOneWidget);
-    // });
+      // ignore: lines_longer_than_80_chars
+      // testWidgets("Justin's Bird of the Day text is displayed", (tester) async {
+      //   expect(find.text('Bird of the Day'), findsOneWidget);
+      // });
 
-    // testWidgets('Black Fronted Bushsrike text is displayed', (tester) async {
-    //   await tester.pumpWidget(const MaterialApp(
-    //     home: Scaffold(
-    //       body: Home(),
-    //     ),
-    //   ));
-    //   expect(find.text('Black Fronted Bushsrike'), findsOneWidget);
-    // });
+      // testWidgets('Black Fronted Bushsrike text is displayed', (tester) async {
+      //   await tester.pumpWidget(const MaterialApp(
+      //     home: Scaffold(
+      //       body: Home(),
+      //     ),
+      //   ));
+      //   expect(find.text('Black Fronted Bushsrike'), findsOneWidget);
+      // });
 
-    testWidgets('View Map button is displayed', (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: Home(),
-        ),
-      ));
-      expect(find.text('View Map'), findsOneWidget);
-    });
+      testWidgets('View Map button is displayed', (tester) async {
+        await tester.pumpWidget(const MaterialApp(
+          home: Scaffold(
+            body: Home(),
+          ),
+        ));
+        expect(find.text('View Map'), findsOneWidget);
+      });
 
-    testWidgets('Bottom navigation is displayed', (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: Home(),
-        ),
-      ));
-      expect(find.byType(BottomNavigation), findsOneWidget);
-    });
-  });
+      testWidgets('Bottom navigation is displayed', (tester) async {
+        await tester.pumpWidget(const MaterialApp(
+          home: Scaffold(
+            body: Home(),
+          ),
+        ));
+        expect(find.byType(BottomNavigation), findsOneWidget);
+      });
+
+      testWidgets(
+        'Search is displayed',
+        (tester) async {
+          await tester.pumpWidget(
+            const MaterialApp(
+              home: Scaffold(
+                body: Home(),
+              ),
+            ),
+          );
+          expect(find.byType(SearchbarContainer), findsOneWidget);
+        },
+      );
+    },
+  );
 }
