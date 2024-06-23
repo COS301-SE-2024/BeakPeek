0770f21c6d394c4727d3459276449be03ce138b2
JarodJeffery <JarodJeffery@gmail.com>
2024-06-23 15:20:14 +0200

    Dark light working

--- beakpeek/lib/View/UserProfile/user_profile.dart
+++ beakpeek/lib/View/UserProfile/user_profile.dart
@@ -1,5 +1,6 @@
+import 'package:beakpeek/Model/user_profile_function.dart';
 import 'package:flutter/material.dart';
-import 'package:localstorage/localstorage.dart';
+import 'package:slider_button/slider_button.dart';
 
 class UserProfile extends StatefulWidget {
   const UserProfile({required this.change, super.key});
@@ -10,17 +11,46 @@ class UserProfile extends StatefulWidget {
 }
 
 class UserProfileState extends State<UserProfile> {
+  Widget iconDisplay = getIcon();
+  String iconLabel = getLabelIcon();
+  @override
+  void initState() {
+    iconDisplay = getIcon();
+    iconLabel = getLabelIcon();
+    super.initState();
+  }
+
+  void changeIcon() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('The mode is ${localStorage.getItem('theme') ?? 'light'}'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          widget.change();
-        },
-        child: const Icon(Icons.add),
+        child: Column(
+          children: [
+            //profile photo
+            //fullname
+            //livelist
+            Center(
+              child: SliderButton(
+                icon: Center(
+                  child: iconDisplay,
+                ),
+                action: () async {
+                  widget.change();
+                  setState(() {
+                    iconDisplay = getIcon();
+                    iconLabel = getLabelIcon();
+                  });
+                  return false;
+                },
+                label: Text(iconLabel),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
