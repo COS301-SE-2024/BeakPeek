962062b05f70a99774200b4eb0f20d265ae3c3d0
ChuufMaster <ivanhorak20@gmail.com>
2024-06-23 10:58:16 +0200

    Added all the Models for birds, apbstracted the database, and added an importer class

---
+++ dotnet/BeakPeekApi/Controllers/BirdController.cs
@@ -0,0 +1,139 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BeakPeekApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeakPeekApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BirdController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BirdController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bird>>> GetBirds()
+        {
+            return await _context.Birds.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bird>> GetBird(int id)
+        {
+            var species = await _context.Birds.FindAsync(id);
+
+            if (species == null)
+            {
+                return NotFound();
+            }
+
+            return species;
+        }
+
+        [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<Bird>>> SearchBirdSpecies(string genus = null, string commonSpecies = null)
+        {
+            IQueryable<Bird> query = _context.Birds;
+
+            if (!string.IsNullOrEmpty(genus))
+            {
+                query = query.Where(b => EF.Functions.Like(b.Genus, $"%{genus}%"));
+            }
+
+            if (!string.IsNullOrEmpty(commonSpecies))
+            {
+                query = query.Where(b => EF.Functions.Like(b.Common_species, $"%{commonSpecies}%"));
+            }
+
+            var results = await query.ToListAsync();
+
+            if (!results.Any())
+            {
+                return NotFound();
+            }
+            return results;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Bird>> PostBirds(Bird species)
+        {
+            _context.Birds.Add(species);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBirds), new { id = species.Pentad }, species);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBird(string id, Bird species)
+        {
+            if (id != species.Pentad)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(species).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BirdExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBird(int id)
+        {
+            var species = await _context.Birds.FindAsync(id);
+            if (species == null)
+            {
+                return NotFound();
+            }
+
+            _context.Birds.Remove(species);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BirdExists(string id)
+        {
+            return _context.Birds.Any(e => e.Pentad == id);
+        }
+
+        [HttpGet("{pentad}/pentad")]
+        public async Task<ActionResult<IEnumerable<Bird>>> GetBirdsInPentad(string pentad)
+        {
+
+            var pentadBirdList = await _context.Birds
+                                            .Where(s => s.Pentad == pentad)
+                                            .ToListAsync();
+
+            if (pentadBirdList == null || pentadBirdList.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(pentadBirdList);
+        }
+    }
+}
---
+++ dotnet/BeakPeekApi/Helpers/CsvImporter.cs
@@ -0,0 +1,91 @@
+using System.Globalization;
+using BeakPeekApi.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace BeakPeekApi.Helpers
+{
+
+    public class CsvImporter
+    {
+        private readonly AppDbContext _context;
+
+        public CsvImporter(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual void ImportCsvData(string filepath, string provinceName)
+        {
+            var province = _context.Provinces.FirstOrDefault(p => p.Name == provinceName);
+
+            if (province == null)
+            {
+                // Console.WriteLine("Adding new province");
+                province = new Province { Name = provinceName };
+                _context.Provinces.Add(province);
+                _context.SaveChanges();
+            }
+            else
+            {
+                // Console.WriteLine($"Province with name: {provinceName}");
+            }
+
+            using (var reader = new StreamReader(filepath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                csv.Context.RegisterClassMap<BirdRecordMap>();
+                var records = csv.GetRecords<Bird>().ToList();
+
+                foreach (var record in records)
+                {
+                    record.ProvinceId = province.Id;
+                }
+
+                _context.Birds.AddRange(records);
+                _context.SaveChanges();
+            }
+        }
+
+        public virtual void ClearProvinceData(string provinceName)
+        {
+            var province = _context.Provinces.FirstOrDefault(p => p.Name == provinceName);
+            if (province != null)
+            {
+                var records = _context.Birds.Where(r => r.ProvinceId == province.Id).ToList();
+                // Console.WriteLine($"Found {records.Count} records to remove for province: {provinceName}");
+                _context.Birds.RemoveRange(records);
+                _context.SaveChanges();
+                // Console.WriteLine("Records removed and changes saved.");
+            }
+            else
+            {
+                // Console.WriteLine($"No province found with name: {provinceName}");
+            }
+        }
+
+        public virtual void ReplaceProvinceData(string filepath, string provinceName)
+        {
+            ClearProvinceData(provinceName);
+            ImportCsvData(filepath, provinceName);
+        }
+    }
+
+    public sealed class BirdRecordMap : ClassMap<Bird>
+    {
+        public BirdRecordMap()
+        {
+            Map(m => m.Pentad).Index(0);
+            Map(m => m.Spp).Index(1);
+            Map(m => m.Common_group).Index(2);
+            Map(m => m.Common_species).Index(3);
+            Map(m => m.Genus).Index(4);
+            Map(m => m.Species).Index(5);
+            Map(m => m.Total_Records).Index(18);
+            Map(m => m.Total_Cards).Index(19);
+            Map(m => m.ReportingRate).Index(20);
+            Map(m => m.ProvinceId).Ignore();
+            Map(m => m.Province).Ignore();
+        }
+    }
+}
