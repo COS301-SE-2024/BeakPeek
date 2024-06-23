64032758afacbe301a690725dbdd10883e27c278
ChuufMaster <ivanhorak20@gmail.com>
2024-06-23 17:28:56 +0200

    Coverd ImportAllCsv with a test

--- dotnet/BeakPeekApi.Tests/Helpers/CsvImporterTest.cs
+++ dotnet/BeakPeekApi.Tests/Helpers/CsvImporterTest.cs
@@ -117,5 +117,26 @@ public class CsvImporterTests
         Assert.Equal(27.3, birdRecord.ReportingRate);
     }
 
+    [Fact]
+    public void ImportAllCsvData_ShouldCallImportCsvDataForEachCsvFile()
+    {
+        // Arrange
+        var directoryPath = "../../../testData/";
+        var csvFiles = Directory.GetFiles(directoryPath, "*.csv");
+
+
+        // Act
+        _csvImporter.ImportAllCsvData(directoryPath);
+
+        // Assert
+        foreach (var csvFile in csvFiles)
+        {
+            var province = Path.GetFileNameWithoutExtension(csvFile);
+            var provinceInDb = _context.Provinces.FirstOrDefault(p => p.Name == province);
+
+            Assert.NotNull(provinceInDb);
+            Assert.Equal(49, provinceInDb.Birds.Count());
+        }
+    }
 }
 
