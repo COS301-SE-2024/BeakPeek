8e0873b4fab1c47ea88b4697ea3f0b2c6e8fb00a
JarodJeffery <JarodJeffery@gmail.com>
2024-07-15 16:27:47 +0200

    Go router

---
+++ beakpeek/lib/Controller/Main/routing_data.dart
@@ -0,0 +1,40 @@
+import 'package:beakpeek/View/Home/home.dart';
+import 'package:beakpeek/View/Home/map_info.dart';
+import 'package:beakpeek/View/Login/landing_page.dart';
+import 'package:beakpeek/View/UserProfile/user_profile.dart';
+import 'package:go_router/go_router.dart';
+
+class RoutingData {
+  final GoRouter router = GoRouter(
+    initialLocation: '/',
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/map',
+        builder: (context, state) {
+          return const MapInfo();
+        },
+      ),
+      GoRoute(
+        path: '/profile',
+        builder: (context, state) {
+          final Function() changeT = state.extra! as Function();
+          return UserProfile(change: changeT);
+        },
+      ),
+      GoRoute(
+        path: '/',
+        builder: (context, state) {
+          return const LandingPage();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            path: 'home',
+            builder: (context, state) {
+              return const Home();
+            },
+          ),
+        ],
+      ),
+    ],
+  );
+}
