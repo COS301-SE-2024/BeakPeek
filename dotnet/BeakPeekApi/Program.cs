2ff5d2d8489acd7c10abe4af0b63c4869d90de07
Justin Hudson <133124412+u21543152@users.noreply.github.com>
2024-07-22 10:34:16 +0200

    Added functionality to retrieve the info and images of birds

--- dotnet/BeakPeekApi/Program.cs
+++ dotnet/BeakPeekApi/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
 
 var connection = String.Empty;
 
