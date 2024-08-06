ca64664d5329c064e27a877f579a631c703b0ddf
Justin Hudson <u21543152@tuks.co.za>
2024-08-06 21:26:52 +0200

    Updated home.test

--- beakpeek/test/home_test.dart
+++ beakpeek/test/home_test.dart
@@ -1,150 +1,44 @@
-// ignore_for_file: lines_longer_than_80_chars
-
-import 'package:beakpeek/Model/nav.dart';
 import 'package:beakpeek/View/Home/Searching/searchbar_container.dart';
-import 'package:beakpeek/View/Home/home.dart';
-import 'package:beakpeek/View/Home/map_info.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:beakpeek/View/Home/home.dart';
+import 'package:beakpeek/Model/nav.dart';
 
 void main() {
-  group(
-    'Home widget test',
-    () {
-      testWidgets('View Map button is displayed', (tester) async {
-        await tester.pumpWidget(const MaterialApp(
-          home: Scaffold(
-            body: Home(),
-          ),
-        ));
-        expect(find.text('View Map'), findsAtLeast(1));
-      });
-      testWidgets('Bottom navigation is displayed', (tester) async {
-        await tester.pumpWidget(const MaterialApp(
-          home: Scaffold(
-            body: Home(),
-          ),
-        ));
-        expect(find.byType(BottomNavigation), findsOneWidget);
-      });
+  testWidgets('Home widget displays all sections correctly', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Home()));
 
-      testWidgets(
-        'Help Icon',
-        (tester) async {
-          await tester.pumpWidget(
-            const MaterialApp(
-              home: Scaffold(
-                body: Home(),
-              ),
-            ),
-          );
-          expect(find.byKey(const Key('helpLogo')), findsAtLeast(1));
-        },
-      );
+    // Verify that the SearchbarContainer is present
+    expect(find.byType(SearchbarContainer), findsOneWidget);
 
-      // testWidgets(
-      //   'Help Icon tap',
-      //   (tester) async {
-      //     await tester.pumpWidget(
-      //       const MaterialApp(
-      //         home: Scaffold(
-      //           body: Home(),
-      //         ),
-      //       ),
-      //     );
-      //     expect(find.byKey(const Key('helpLogo')), findsAtLeast(1));
-      //     await tester.tap(find.byKey(const Key('helpLogo')));
-      //     await tester.pumpAndSettle();
-      //     expect(find.byType(AlertDialog), findsOne);
-      //   },
-      // );
+    // Verify that the Quiz section is present
+    expect(find.text('Test Your Knowledge!'), findsOneWidget);
+    expect(find.text('Guess the bird from the picture...'), findsOneWidget);
+    expect(find.text('Start Quiz'), findsOneWidget);
 
-      testWidgets(
-        'Map button tap',
-        (tester) async {
-          await tester.pumpWidget(
-            MaterialApp(
-              onGenerateRoute: (routeSettings) {
-                return TestRouting.onGenerateRoute(
-                  settings: routeSettings,
-                  routeStubbingOptions: {Routing.loginRoute: true},
-                );
-              },
-              home: const Scaffold(
-                body: Home(),
-              ),
-            ),
-          );
-          await tester.tap(find.byType(FilledButton));
-          await tester.pumpAndSettle();
+    // Verify that the Achievements section is present
+    expect(find.text('Tracked Achievements'), findsOneWidget);
+    expect(find.text('Achievement 1'), findsOneWidget);
+    expect(find.text('Achievement 2'), findsOneWidget);
 
-          expect(find.byKey(const Key(Routing.loginRoute)), findsNothing);
-        },
-      );
+    // Verify that the Bird Wishlist section is present
+    expect(find.text('Birds You Want to See'), findsOneWidget);
+    expect(find.text('Brown-Headed Parrot'), findsOneWidget);
+    expect(find.text('Cape Vulture'), findsOneWidget);
+    expect(find.text('Honeyguide'), findsOneWidget);
 
-      testWidgets(
-        'Search is displayed',
-        (tester) async {
-          await tester.pumpWidget(
-            const MaterialApp(
-              home: Scaffold(
-                body: Home(),
-              ),
-            ),
-          );
-          expect(find.byType(SearchbarContainer), findsOneWidget);
-        },
-      );
-    },
-  );
-}
+    // Verify that the BottomNavigation is present
+    expect(find.byType(BottomNavigation), findsOneWidget);
+  });
 
-typedef RouteStubbingOptions = Map<String, bool>;
+  testWidgets('Start Quiz button can be tapped', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: Home()));
 
-class TestRouting {
-  static PageRoute onGenerateRoute({
-    required RouteSettings settings,
-    required RouteStubbingOptions routeStubbingOptions,
-  }) {
-    final routeName = settings.name;
-    return MaterialPageRoute(
-      builder: (_) {
-        final routeStubbingFlag = routeStubbingOptions[routeName] ?? false;
+    // Tap the "Start Quiz" button
+    await tester.tap(find.text('Start Quiz'));
+    await tester.pump();
 
-        final Key key = Key(settings.name!);
-
-        return routeStubbingFlag
-            ? Placeholder(key: key)
-            : Routing.getWidgetOfRoute(settings);
-      },
-      settings: settings,
-    );
-  }
-}
-
-class Routing {
-  static const String loginRoute = '/map';
-  static const String homeRoute = '/home';
-
-  static Map<String, Widget Function(RouteSettings)> routeToWidgetMappings = {
-    loginRoute: (settings) => const Home(),
-    homeRoute: (settings) => const MapInfo(),
-  };
-
-  static Widget getWidgetOfRoute(RouteSettings settings) {
-    final routeName = settings.name;
-    final widgetFunction = routeToWidgetMappings[routeName];
-    assert(
-      widgetFunction != null,
-      "No route to widget mapping found for route \"${settings.name}\", make sure you have registered the widget as you're adding a new route.",
-    );
-    return widgetFunction!(settings);
-  }
-
-  static PageRoute onGenerateRoute(RouteSettings settings) {
-    return MaterialPageRoute(
-      builder: (context) => getWidgetOfRoute(settings),
-      settings: settings,
-    );
-  }
+    // Verify some expected behavior here, e.g., navigation or showing a quiz screen
+    // This part is dependent on the actual functionality you want to test
+  });
 }
