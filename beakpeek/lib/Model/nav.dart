d64e8ebc3e91e984cbab95db64c1aa665a6c96b6
Matt <mattgbotha@gmail.com>
2024-09-02 10:48:40 +0200

    Navbar works in dark mode

--- beakpeek/lib/Model/nav.dart
+++ beakpeek/lib/Model/nav.dart
@@ -1,3 +1,4 @@
+import 'package:beakpeek/Styles/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:beakpeek/config_azure.dart' as config;
@@ -30,6 +31,8 @@ class BottomNavItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final iconFile = isDarkMode ? file.replaceAll('.png', 'Dark.png') : file;
     return SizedBox(
       width: itemWidth,
       child: Column(
@@ -48,15 +51,15 @@ class BottomNavItem extends StatelessWidget {
               }
             },
             child: Image.asset(
-              'assets/icons/$file',
+              'assets/icons/$iconFile',
               width: itemWidth * 0.5,
             ),
           ),
           const SizedBox(height: 1),
           Text(
             label,
-            style: const TextStyle(
-              color: Colors.black,
+            style: TextStyle(
+              color: AppColors.greyColor(context),
               fontSize: 14,
               fontFamily: 'SF Pro Display',
               fontWeight: FontWeight.w500,
