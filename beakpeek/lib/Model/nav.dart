aade4923fbfaaf25a98665b0192f47a2a3e50a47
Matt <mattgbotha@gmail.com>
2024-09-09 21:56:30 +0200

    🔥 Removed unecessary code

--- beakpeek/lib/Model/nav.dart
+++ beakpeek/lib/Model/nav.dart
@@ -1,4 +1,3 @@
-import 'package:beakpeek/Styles/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:beakpeek/config_azure.dart' as config;
