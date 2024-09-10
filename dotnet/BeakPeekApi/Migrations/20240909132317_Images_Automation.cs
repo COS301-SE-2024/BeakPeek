ad4fc07340ea1ae60ba366ff2dc82974dbe1ce2a
ChuufMaster <ivanhorak20@gmail.com>
2024-09-10 10:37:46 +0200

    🔨Added migration script for bird images

---
+++ dotnet/BeakPeekApi/Migrations/20240909132317_Images_Automation.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeakPeekApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Images_Automation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image_url",
+                table: "Birds",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Info",
+                table: "Birds",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image_url",
+                table: "Birds");
+
+            migrationBuilder.DropColumn(
+                name: "Info",
+                table: "Birds");
+        }
+    }
+}
