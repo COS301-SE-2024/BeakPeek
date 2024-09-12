2557ef024e45b1376f496dafad925480f691cabc
ChuufMaster <ivanhorak20@gmail.com>
2024-09-12 13:10:11 +0200

    ✨Feat: Added a mock class and DbContextMock class
    - Added Mock class for uniformly mocking out dependencies
    - Added a DbContextMock to better mock out the database

--- dotnet/BeakPeekApi.Tests/Controllers/BirdControllerTest.cs
+++ dotnet/BeakPeekApi.Tests/Controllers/BirdControllerTest.cs
@@ -7,104 +7,32 @@ public class BirdControllerTest
 {
     private readonly AppDbContext _context;
     private readonly BirdController _controller;
+    private readonly BirdImageHelper _birdImageHelper;
+    private readonly BirdInfoHelper _birdInfoHelper;
+    private readonly HttpClient _httpClient;
+    private readonly GeneralHelper _generalHelper;
+
+    private readonly BlobStorageService _blobStorageService;
 
     public BirdControllerTest()
     {
-        var options = new DbContextOptionsBuilder<AppDbContext>()
-            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            .Options;
-        _context = new AppDbContext(options);
-        _controller = new BirdController(_context);
+        _context = DbContextMock.GetDbContex();
 
-        SeedDatabase();
+        _httpClient = Mocks.GetHttpClient();
+
+        _generalHelper = new GeneralHelper(Mocks.GetConfiguration());
+
+        _blobStorageService = new BlobStorageService(_generalHelper, Mocks.GetBlobContainerClient("image-helper-test-container"));
+
+        _birdInfoHelper = new BirdInfoHelper(_httpClient, _generalHelper);
+        _birdImageHelper = new BirdImageHelper(_birdInfoHelper, _context, _httpClient, _blobStorageService);
+        _controller = new BirdController(_context, _birdImageHelper);
+
     }
 
     private void Dispose()
     {
         _context.Dispose();
-    }
-
-    private void SeedDatabase()
-    {
-        // Clear the database
-        // _context.Birds.RemoveRange(_context.Birds);
-        // _context.Provinces.RemoveRange(_context.Provinces);
-        // _context.SaveChanges();
-
-        var provinces = new List<ProvinceList> {
-                new ProvinceList { Id = 1, Name = "easterncape", Province_Birds = { } },
-                new ProvinceList { Id = 2, Name = "freestate", Province_Birds = { } },
-                new ProvinceList { Id = 3, Name = "gauteng", Province_Birds = { } },
-                new ProvinceList { Id = 4, Name = "kwazulunatal", Province_Birds = { } },
-                new ProvinceList { Id = 5, Name = "limpopo", Province_Birds = { } },
-                new ProvinceList { Id = 6, Name = "mpumalanga", Province_Birds = { } },
-                new ProvinceList { Id = 7, Name = "northerncape", Province_Birds = { } },
-                new ProvinceList { Id = 8, Name = "northwest", Province_Birds = { } },
-                new ProvinceList { Id = 9, Name = "westerncape", Province_Birds = { } }
-        };
-
-        var pentads = new List<Pentad>
-        {
-            new Pentad {Pentad_Allocation="1_1", Province = provinces[0], Total_Cards = 1, Pentad_Latitude = 1, Pentad_Longitude = 1 },
-            new Pentad {Pentad_Allocation="2_2", Province = provinces[1], Total_Cards = 2, Pentad_Latitude = 2, Pentad_Longitude = 2 }
-        };
-
-        var birds = new List<Bird>
-        {
-            new Bird
-            {
-                Genus="Genus1",
-                Species="Species1",
-                Common_group="CommonGroup1",
-                Common_species="CommonSpecies1",
-                Ref=1,
-                Latest_FP=null,
-                Bird_Provinces= new List<ProvinceList> {provinces[0]},
-                Full_Protocol_RR=1.1,
-                Full_Protocol_Number=1
-            },
-
-            new Bird
-            {
-                Genus="Genus2",
-                Species="Species2",
-                Common_group="CommonGroup2",
-                Common_species="CommonSpecies2",
-                Ref=2,
-                Latest_FP=null,
-                Bird_Provinces= new List<ProvinceList> {provinces[1]},
-                Full_Protocol_RR=2.2,
-                Full_Protocol_Number=2
-            },
-        };
-
-        var provinces_1 = new List<Easterncape>
-        {
-            new Easterncape { Pentad = pentads[0], Bird = birds[0],ReportingRate=1.1,Total_Records=1},
-
-            new Easterncape { Pentad = pentads[1], Bird = birds[1],ReportingRate=2.2,Total_Records=2},
-        };
-
-
-        var provinces_2 = new List<Freestate>
-        {
-            new Freestate { Pentad = pentads[0], Bird = birds[0],ReportingRate=1.1,Total_Records=1},
-
-            new Freestate { Pentad = pentads[1], Bird = birds[1],ReportingRate=2.2,Total_Records=2},
-        };
-
-
-
-        _context.ProvincesList.AddRange(provinces);
-        _context.SaveChanges();
-        _context.Birds.AddRange(birds);
-        _context.SaveChanges();
-        _context.Pentads.AddRange(pentads);
-        _context.SaveChanges();
-        _context.Easterncape.AddRange(provinces_1);
-        _context.SaveChanges();
-        _context.Freestate.AddRange(provinces_2);
-        _context.SaveChanges();
     }
 
     [Fact]
