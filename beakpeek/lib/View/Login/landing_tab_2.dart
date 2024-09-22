56a0fcb75988a3f93beb3f65f35e4d5f98af374c
Matt <mattgbotha@gmail.com>
2024-09-22 12:29:26 +0200

    💄 Changed landing tab images to local bird

--- beakpeek/lib/View/Login/landing_tab_2.dart
+++ beakpeek/lib/View/Login/landing_tab_2.dart
@@ -12,103 +12,118 @@ class LandingTab2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: AppColors.backgroundColor(context),
       body: SafeArea(
-        child: Column(
+        child: Stack(
           children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Spacer(flex: (screenHeight * 0.02).toInt()),
-                  Align(
-                    alignment: const Alignment(-0.6, 0.7),
-                    child: RichText(
-                      textAlign: TextAlign.left,
-                      text: TextSpan(
-                        style: GlobalStyles.mainHeadingPrimary(context),
-                        children: [
-                          TextSpan(
-                              text: 'Explore,\n',
-                              style: GlobalStyles.mainHeadingPrimary(context)),
-                          TextSpan(
-                            text: 'Discover,\n',
-                            style: GlobalStyles.mainHeadingTertiary(context),
-                          ),
-                          TextSpan(
-                              text: 'and Share',
-                              style: GlobalStyles.mainHeadingPrimary(context)),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: screenHeight * 0.02),
-                  Align(
-                    alignment: const Alignment(-0.5, 0.0),
-                    child: RichText(
-                      text: TextSpan(
-                        style: GlobalStyles.contentPrimary(context),
-                        children: const <TextSpan>[
-                          TextSpan(
-                            text: 'Create an account or sign in \nto join the',
-                          ),
-                          TextSpan(
-                            text: ' BeakPeak',
-                            style: TextStyle(
-                              color: Color.fromARGB(255, 216, 96, 48),
-                              fontSize: 18,
-                              fontFamily: 'SF Pro Display',
-                            ),
-                          ),
-                          TextSpan(
-                            text: ' community.',
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Spacer(flex: (screenHeight * 0.03).toInt()),
-                  OutlinedButton(
-                    style: GlobalStyles.buttonPrimaryFilled(context),
-                    child: Text('Sign Up / Sign In',
-                        style: GlobalStyles.primaryButtonText(context)),
-                    onPressed: () {
-                      if (localStorage.getItem('termsAndCondition') == 'true') {
-                        loginFunction(context);
-                      } else {
-                        showPopupCard(
-                          dimBackground: true,
-                          context: context,
-                          builder: (context) {
-                            return PopupCard(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                              ),
-                              child: TermsAndConditionsPopup(),
-                            );
-                          },
-                        );
-                      }
-                    },
-                  ),
-                  SizedBox(height: screenHeight * 0.02),
-                  FilledButton(
-                    style: GlobalStyles.buttonPrimaryOutlined(context),
-                    child: Text('Sign In as Guest',
-                        style: GlobalStyles.secondaryButtonText(context)),
-                    onPressed: () {
-                      context.go('/home');
-                    },
-                  ),
-                ],
+            Positioned(
+              top: screenHeight * 0.25,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                'assets/images/landing1.png',
+                width: screenWidth,
+                height: screenHeight * 0.8,
+                fit: BoxFit.cover,
               ),
             ),
-            Image.asset(
-              'assets/images/landing2.png',
-              height: screenHeight * 0.45,
-              width: double.infinity,
-              fit: BoxFit.contain,
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: EdgeInsets.only(top: screenHeight * 0.07),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Align(
+                      alignment: const Alignment(-0.6, 0),
+                      child: RichText(
+                        textAlign: TextAlign.left,
+                        text: TextSpan(
+                          style: GlobalStyles.mainHeadingPrimary(context),
+                          children: [
+                            TextSpan(
+                                text: 'Explore,\n',
+                                style:
+                                    GlobalStyles.mainHeadingPrimary(context)),
+                            TextSpan(
+                              text: 'Discover,\n',
+                              style: GlobalStyles.mainHeadingTertiary(context),
+                            ),
+                            TextSpan(
+                                text: 'and Share',
+                                style:
+                                    GlobalStyles.mainHeadingPrimary(context)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    Align(
+                      alignment: const Alignment(-0.5, 0.0),
+                      child: RichText(
+                        text: TextSpan(
+                          style: GlobalStyles.contentPrimary(context),
+                          children: const <TextSpan>[
+                            TextSpan(
+                              text:
+                                  'Create an account or sign in \nto join the',
+                            ),
+                            TextSpan(
+                              text: ' BeakPeak',
+                              style: TextStyle(
+                                color: Color.fromARGB(255, 216, 96, 48),
+                                fontSize: 18,
+                                fontFamily: 'SF Pro Display',
+                              ),
+                            ),
+                            TextSpan(
+                              text: ' community.',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    OutlinedButton(
+                      style: GlobalStyles.buttonPrimaryFilled(context),
+                      child: Text('Sign Up / Sign In',
+                          style: GlobalStyles.primaryButtonText(context)),
+                      onPressed: () {
+                        if (localStorage.getItem('termsAndCondition') ==
+                            'true') {
+                          loginFunction(context);
+                        } else {
+                          showPopupCard(
+                            dimBackground: true,
+                            context: context,
+                            builder: (context) {
+                              return PopupCard(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12.0),
+                                ),
+                                child: TermsAndConditionsPopup(),
+                              );
+                            },
+                          );
+                        }
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    FilledButton(
+                      style: GlobalStyles.buttonPrimaryOutlined(context),
+                      child: Text('Sign In as Guest',
+                          style: GlobalStyles.secondaryButtonText(context)),
+                      onPressed: () {
+                        context.go('/home');
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
