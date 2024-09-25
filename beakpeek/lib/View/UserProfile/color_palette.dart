ba2e3a0f59aa27ea4636a2616ab7db07791fdae4
Justin Hudson <u21543152@tuks.co.za>
2024-09-25 22:54:13 +0200

    added the colour scheme functionality and legend to the heat map

--- beakpeek/lib/View/UserProfile/color_palette.dart
+++ beakpeek/lib/View/UserProfile/color_palette.dart
@@ -1,43 +1,138 @@
-// ignore_for_file: library_private_types_in_public_api
-
-import 'package:beakpeek/Controller/Main/color_palette_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:beakpeek/Model/Globals/globals.dart';
+import 'package:beakpeek/Styles/global_styles.dart';
+import 'package:beakpeek/Styles/colors.dart';
+import 'package:beakpeek/Controller/Main/color_palette_functions.dart';
 
-class PaletteSelector extends StatefulWidget {
-  const PaletteSelector({super.key});
-
-  @override
-  _PaletteSelectorState createState() => _PaletteSelectorState();
-}
-
-class _PaletteSelectorState extends State<PaletteSelector> {
-  ColorPalette selectedPalette = greenRedPalette; // Default palette
-
+class PaletteSelector extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Select a Color Palette:'),
-        DropdownButton<ColorPalette>(
-          value: selectedPalette,
-          items: [
-            DropdownMenuItem(
-              value: greenRedPalette,
-              child: const Text('Green-Red'),
-            ),
-            DropdownMenuItem(
-              value: bluePurplePalette,
-              child: const Text('Blue-Purple'),
-            ),
-            // Add more palette options here
-          ],
-          onChanged: (newPalette) {
-            setState(() {
-              selectedPalette = newPalette!;
-            });
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor(context),
+      appBar: AppBar(
+        backgroundColor: AppColors.backgroundColor(context),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: AppColors.iconColor(context)),
+          onPressed: () {
+            Navigator.pop(context); // Go back to the previous screen
           },
         ),
+        title: Text(
+          'Select Color Palette',
+          style: GlobalStyles.smallHeadingPrimary(context),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10.0),
+        children: [
+          _buildPaletteOption(context, greenRedPalette, 'Green-Red'),
+          _buildPaletteOption(context, bluePurplePalette, 'Blue-Purple'),
+          _buildPaletteOption(context, earthyPalette, 'Earthy'),
+          _buildPaletteOption(context, sunsetPalette, 'Sunset'),
+          _buildPaletteOption(context, oceanPalette, 'Ocean'),
+          _buildPaletteOption(context, tropicalPalette, 'Tropical'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildPaletteOption(
+      BuildContext context, ColorPalette palette, String title) {
+    return GestureDetector(
+      onTap: () {
+        // Update the global palette
+        global.palette = palette;
+        Navigator.pop(context); // Go back after selecting
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 8.0),
+        decoration: BoxDecoration(
+          color: AppColors.popupColor(context),
+          borderRadius: BorderRadius.circular(15.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 3,
+              offset: const Offset(0, 1),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: GlobalStyles.contentPrimary(context),
+              ),
+              const SizedBox(height: 10),
+              Center(
+                child: _buildColorSwatches(palette, context),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColorSwatches(ColorPalette palette, BuildContext context) {
+    // Get the screen width
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Center(
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        // Left label
+        Text(
+          'Low',
+          style: TextStyle(
+              fontSize: screenWidth * 0.03,
+              color: AppColors.primaryColor(
+                  context)), // Adjust font size based on screen width
+        ),
+        // Color swatches
+        Center(
+          child: Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                _buildColorSwatch(palette.low, context), // No label
+                _buildColorSwatch(palette.mediumLow, context), // No label
+                _buildColorSwatch(palette.medium, context), // No label
+                _buildColorSwatch(palette.mediumHigh, context), // No label
+                _buildColorSwatch(palette.high, context), // No label
+                _buildColorSwatch(palette.veryHigh, context), // No label
+              ],
+            ),
+          ),
+        ),
+        // Right label
+        Text(
+          'Very High',
+          style: TextStyle(
+            fontSize: screenWidth * 0.03,
+            color: AppColors.primaryColor(
+                context), // Adjust font size based on screen width
+          ),
+        )
       ],
+    ));
+  }
+
+  Widget _buildColorSwatch(Color color, BuildContext context) {
+    // Get the screen width
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: screenWidth * 0.11, // Width as a percentage of screen width
+      height: screenWidth * 0.05, // Height as a percentage of screen width
+      color: color,
     );
   }
 }
