cfb80fada9b4d3d5530c3dcd425f33ea7666695d
ChuufMaster <ivanhorak20@gmail.com>
2024-09-28 15:06:56 +0200

    ✨ Feat: Added the ability to either use an icon name or byte array as image display

---
+++ dotnet/UserApi/UserApi/Migrations/20240928125838_AchievementIcons.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AchievementIcons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                schema: "Identity",
+                table: "Achievement",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Iconname",
+                schema: "Identity",
+                table: "Achievement",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                schema: "Identity",
+                table: "Achievement");
+
+            migrationBuilder.DropColumn(
+                name: "Iconname",
+                schema: "Identity",
+                table: "Achievement");
+        }
+    }
+}
